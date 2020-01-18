1c009d67175c2658af6454299bb4533c7021e38e
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2020-01-18 16:35:06 +0530

    The structure sql queries are uploaded

---
+++ mysql.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE social_media;
+
+USE social_media;
+
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    college VARCHAR(255),
+    phone_no VARCHAR(255),
+    PRIMARY KEY(id)
+    );
+    
+
+CREATE TABLE statuses(
+    id INT NOT NULL AUTO_INCREMENT,
+    date_time DATETIME NOT NULL,
+    status VARCHAR(1000) NOT NULL,
+    user_id INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);     
