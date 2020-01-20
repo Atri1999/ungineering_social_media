73df2c534c9d941d862e21edb576c4a0f7bef558
Atri Chakraborty <chakrabortyatri1999@gmail.com>
2020-01-20 19:44:25 +0530

    status.php  submitted

--- homepage.php
+++ homepage.php
@@ -1,23 +1,39 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>sm_homepage</title>
+        <title>Homepage</title>
         <link rel="stylesheet" href="css/homepage.css"/>
+        <link rel="stylesheet" href="css/hp_login.css"/>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
     </head>
     <body>
         
         <div class="header">
             <div class="logo">
-                <div class="c1"><img src="img/ungineering_logo.svg" class="imag"/></div>
-                <div class="c2">
+                <a href="http://127.0.0.1/social_media/homepage.php" class="aaaa" target="_blank">
+                <div>
+                    <div class="c1"><img src="img/ungineering_logo.svg" class="imag"/></div>
+                    <div class="c2">
                         <div class="r1"><span class="hghlgh">un</span>gineering</div>
                         <div class="r2">A <span class="hghlgh">bit</span> of knowledge is good, a <span class="hghlgh">byte</span> is better</div>
-                </div>
+                    </div>
+                </div></a>
             </div>
             <div class="hbutton">
-                <button class="first">Log in</button>
-                <button class="second">New User</button>
+                <?php
+                    session_start();
+                    if(isset($_SESSION['id']) && $_SESSION['id']){
+                    ?>
+                <span class="dash_brd">My Dashboard</span>
+                <button class="first" >Logout</button>
+                    <?php
+                    } else {
+                    ?>
+                <a href="web_login.html" ><button class="first">Log in</button></a>
+                <a href="web_registration.php"><button class="second">New User</button></a>
+                    <?php
+                    }
+                    ?>           
             </div>
         </div>
         
@@ -25,6 +41,23 @@
         
         <div class="status">
             <div class="st_content">
+            <?php
+                session_start();
+                if(isset($_SESSION['id']) && $_SESSION['id']){
+                ?>
+                <div class="put_status">
+                    <div class="instr"> Write Something here</div>
+                    <div>
+                        <form method="post" action="status.php">
+                            <input class="space" name="status" type="text"/><br>
+                            <input class="smt" type="submit" name="submit" value="Post"/>
+                        </form>                        
+                    </div>
+                    
+                </div> 
+                <?php
+                }
+                ?> 
                 <div class="row">
                     <div class="r_nm" >Name</div>
                     <div class="r_cnt">Hello,deletime atuy jksd jdfkad silinnd sljnsdns ksdsdndn sodncm KWJSHADA KHEJ hwdlsjldfc ljdfcsskfcs bb edjjhjjss jhjshhfshfdks hfjhjshfjdhfdjh
