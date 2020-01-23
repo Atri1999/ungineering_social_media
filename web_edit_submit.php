b713bf19d3e24c567496abc0bb779a2a0c7c47c8
Krishna Guchhait <krishnaguchhait17@gmail.com>
2020-01-23 10:13:24 +0530

    editprofile php

---
+++ web_edit_submit.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    $hostname = "127.0.0.1";
+    $username = "root";
+    $db_password = "123456";
+    $db_name = "social_media";
+    $conn = mysqli_connect($hostname,$username,$db_password,$db_name);
+    if(!$conn){
+        die("connection failed: ". mysqli_connect_error());
+    }
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $college=$_POST['college'];
+    $phone_no=$_POST['phone_no'];
+    $id=$_SESSION['id'];
+    $sql="UPDATE users SET name='$name',email='$email',password='$password',college='$college',phone_no='$phone_no' WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    if (!$result) {
+        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+    }
+    echo "Profile update";
+    ?>
+        <a href ="update.php"> click here </a> to continue.
+        <?php
+        
+              
+ 
+    
+    mysqli_close($conn);
+?>
+
--- web_editprofile.php
+++ web_editprofile.php
@@ -34,7 +34,7 @@
                     <div class= "r1_c2_cc2">
                         <div class= "r1_c2_rr1"></div>
                         <div class= "r1_c2_rr2">
-                            <div class= "log"><b>Logout</b></div>
+                            <div class= "log"><a href= "logout.php"><b>Logout</b></a></div>
                         </div>
                         <div class= "r1_c2_rr3"></div>
                     </div>
@@ -61,21 +61,52 @@
                 </div>
             </div>
             <div class= "col8">
+               <?php
+                    session_start();
+                    $hostname = "127.0.0.1";
+                    $username = "root";
+                    $db_password = "123456";
+                    $db_name = "social_media";
+                    $conn = mysqli_connect($hostname,$username,$db_password,$db_name);
+                    if(!$conn){
+                        die("connection failed: ". mysqli_connect_error());
+                    }
+                    $id=$_SESSION['id'];
+                    $sql1="SELECT * FROM users";
+                    $result1 = mysqli_query($conn, $sql1);
+                    if (!$result1) {
+                        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+                    }
+                    while($row=mysqli_fetch_array($result1)){
+                        if($id==$row['id']){
+                             $_SESSION['name']=$row['name'];
+                             $_SESSION['email']=$row['email'];
+                             $_SESSION['password']=$row['password'];
+                             $_SESSION['college']=$row['college'];
+                             $_SESSION['phone_no']=$row['phone_no'];
+                             
+                            
+                        }        
+                    } 
+                    mysqli_close($conn);
+    
+                    
+                ?>
                 <div class= "cp2">
                 <form method = "post" action = "web_edit_submit.php">
-                    <br/><br/><br/><br/>
-                    <b><p6>Name:</p6></b><br/><br/>
-                    <input type="text" name="name" ><br/><br/><br/>
-                    <b><p6>Email:</p6></b><br/><br/>
-                    <input type="text" name="Email"><br/><br/><br/>
-                    <b><p6>Password:</p6></b><br/><br/>
-                    <input type="text" name="password" ><br/><br/><br/>
-                    <b><p6>college:</p6></b><br/><br/>
-                    <input type="text" name="college" ><br/><br/><br/>
-                    <b><p6>Phone Number:</p6></b><br/><br/><br/>
-                    <input type="text" name="phone Number"  ><br/><br/><br/>
-                    <br/><br/>
-                    <div class="update"><p10>Update </p10></div>
+                   <br/><br/><br/><br/>
+                   <b><p6>Name:</p6></b><br/><br/>
+                   <input type="text" name="name" value ="<?php echo $_SESSION["name"];?>"><br/><br/><br/>
+                   <b><p6>Email:</p6></b><br/><br/>
+                   <input type="text" name="email" value ="<?php echo $_SESSION["email"] ;?>"><br/><br/><br/>
+                   <b><p6>Password:</p6></b><br/><br/>
+                   <input type="text" name="password" value ="<?php echo $_SESSION["password"] ;?>"><br/><br/><br/>
+                   <b><p6>College:</p6></b><br/><br/>
+                   <input type="text" name="college" value ="<?php echo $_SESSION["college"] ;?>"><br/><br/><br/>
+                   <b><p6>Phone Number:</p6></b><br/><br/><br/>
+                   <input type="text" name="phone_no"  value ="<?php echo $_SESSION["phone_no"] ;?>"><br/><br/><br/>
+                   <br/><br/>
+                   <input type="submit" value="Update" name="update"/>
                 </form>
                 </div>
              </div>
@@ -84,6 +115,7 @@
         
         
         
+       
         
         <div class= "row4">
             <div class= "c6">
