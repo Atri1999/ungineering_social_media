4c0e290a4b2ebc3f36ce2c75d1d2d4c6115ee83f
Biplab Bhattacharya <biplabmath1998@gmail.com>
2020-01-25 17:51:56 +0530

    modified login_sumit file

--- login_submit.php
+++ login_submit.php
@@ -1,37 +1,51 @@
 <?php
     session_start();
-    $hostname="127.0.01";
-    $username="root";
-    $db_password="123456";
-    $db_name="social_media";
-    $conn=mysqli_connect($hostname,$username,$db_password,$db_name);
-    if(!$conn){
-        die("connection failed: ". mysqli_connect_error());
+    $hostname = "localhost";
+    $username = "root";
+    $db_password = "123456";
+    $database = "social_media";
+
+    $response = array();
+    $conn = mysqli_connect($hostname, $username, $db_password, $database);
+    if (!$conn) {
+        $response['success'] = false;
+        $response['message'] = "Connection failed: " . mysqli_connect_error();
+        echo json_encode($response);
+        exit();
     }
-    
+
     $sql = "SELECT * FROM users";
 
     $result = mysqli_query($conn, $sql);
     if (!$result) {
-        die("Error: " . $sql . "<br>" . mysqli_error($conn));
+        $response['success'] = false;
+        $response['message'] = "Error: " . $sql . "<br>" . mysqli_error($conn);
+        echo json_encode($response);
+        exit();
     }
 
-    $email=$_POST['email'];
-    $password= $_POST['password'];
-    $flag=0;
-    
-    while($row=mysqli_fetch_array($result)){
-        if($row['email']==$email && $row['password']==$password){
-            $flag=1;
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+
+    $flag = 0;
+    while ($row=mysqli_fetch_array($result)) {
+        if ($email == $row['email'] && $pass == $row['password']) {
+            $flag = 1;
             $_SESSION['id']=$row['id'];
             $_SESSION['name']=$row['name'];
-            $_SESSION['id']=$row['id'];
-            header('Location:homepage.php');
             break;
         }
     }
-    if($flag==0){
-        header('Location:web_login.php');     
+    
+    if ($flag == 0) {
+        $response['success'] = false;
+        $response['message'] = "Login failed";
+    } else {
+        $response['success'] = true;
+        $response['message'] = "Hello " . $row['name'];
     }
+
+    echo json_encode($response);
+    mysqli_close($conn);
 ?>
 
